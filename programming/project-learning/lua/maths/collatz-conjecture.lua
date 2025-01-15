faf9cb2bceff0f6b8254d129c2d3ada15965af95
ottojonas <ottobjonas@outlook.com>
2025-01-15 11:48:51 +0000

    lua maths

--- programming/project-learning/lua/maths/collatz-conjecture.lua
+++ programming/project-learning/lua/maths/collatz-conjecture.lua
@@ -1,11 +1,18 @@
+-- Prompt the user to enter a number
 io.write("Enter a number: ")
+
+-- Read the user input and convert it to a number
 local number = tonumber(io.read())
 
+-- Loop until the number becomes 1
 while number ~= 1 do
+    -- If the number is even, divide it by 2
     if number % 2 == 0 then
         number = number // 2
     else
+        -- If the number is odd, multiply it by 3 and add 1
         number = number * 3 + 1
     end
+    -- Print the current value of the number
     print(number)
 end
--- programming/project-learning/lua/maths/russian-peasant-long-multiplication.lua
+++ programming/project-learning/lua/maths/russian-peasant-long-multiplication.lua
@@ -1,21 +1,30 @@
+-- Prompt the user to enter the first operand
 io.write("Enter op1: ")
 local op1 = tonumber(io.read())
 
+-- Prompt the user to enter the second operand
 io.write("Enter op2: ")
 local op2 = tonumber(io.read())
 
+-- Initialize the answer to 0
 local ans = 0
 
+-- Loop until the first operand becomes 0
 while op1 > 0 do
+    -- If the first operand is odd, add the second operand to the answer
     if op1 % 2 ~= 0 then
         ans = ans + op2
         print(ans)
     end
 
+    -- Halve the first operand (discarding the remainder)
     op1 = math.floor(op1 / 2)
+    -- Double the second operand
     op2 = op2 * 2
 
+    -- Print the current values of the operands
     print(op1, op2)
 end
 
+-- Print the final answer
 print("The answer is: ", ans)
